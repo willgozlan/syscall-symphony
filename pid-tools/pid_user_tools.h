580de4a147897cf573603efad2ccfdd061776be3
meagancheese <meagan.konst@wustl.edu>
2022-04-14 16:27:18 -0500

    Update pid_user_tools.h

--- pid-tools/pid_user_tools.h
+++ pid-tools/pid_user_tools.h
@@ -1,10 +1,35 @@
+#pragma once
+
+#include "pid_exists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/syscall.h>
-#include <stdio.h>
 
 #define MAX_PID_LENGTH 10
 
-int add_pid(int argc, char* argv[]);
-int remove_pid(int argc, char* argv[]);
-int pid_exists(int pid);
+#define EXPECTED_NUM_ARGS 2
+
+#define PROGRAM_NAME 0
+#define PID 1
+
+#define PID_FOUND 1
+#define PID_NOT_FOUND 0
+
+#define SUCCESS 0
+#define PRINTF_ERROR -1
+#define FFLUSH_ERROR -2
+#define PID_EXISTS_ERROR -3
+#define FOPEN_ERROR -4
+#define FCLOSE_ERROR -5
+#define USAGE_ERROR -6
+#define ADD_PID_ERROR -7
+#define FPRINTF_ERROR -8
+#define REMOVE_PID_ERROR -9
+
+#define ATOI_RET_ERROR_VALUE 0
+
+int add_pid(int pid);
+int remove_pid(int pid);
