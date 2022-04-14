e48e795971f70af997b6d4992f54b6ca639e402f
meagancheese <meagan.konst@wustl.edu>
2022-04-14 16:26:41 -0500

    Update add_pid.c

--- pid-tools/add_pid.c
+++ pid-tools/add_pid.c
@@ -1,45 +1,45 @@
-#include "add_pid.h"
+#include "pid_user_tools.h"
 
 int add_pid(int pid) {
     int exists = pid_exists(pid);
-    if (exists < 0) {
-        if (printf("Error with pid_exists!\n") < 0) {
-            perror(printf);
+    if (exists < SUCCESS) {
+        if (printf("Error with pid_exists!\n") < SUCCESS) {
+            perror("printf");
             return PRINTF_ERROR;
         }
         if(fflush(stdout)) {
-            perror(fflush);
+            perror("fflush");
             return FFLUSH_ERROR;
         }
         return PID_EXISTS_ERROR;
     }
     else if (exists == PID_FOUND) {
-        if (printf("PID %d is already Musical!\n", pid) < 0) {
-            perror(printf);
+        if (printf("PID %d is already Musical!\n", pid) < SUCCESS) {
+            perror("printf");
             return PRINTF_ERROR;
         }
         if(fflush(stdout)) {
-            perror(fflush);
+            perror("fflush");
             return FFLUSH_ERROR;
         }
         return SUCCESS;
     }
-    else if(exists == PID_NOT_FOUND) {
+    else {
         FILE * pids = fopen(".pids", "a");
         if (pids == NULL) {
-            perror(fopen);
+            perror("fopen");
             return FOPEN_ERROR;
         }
-        if (fprintf(pids, "%d\n", argv[2]) < 0) {
-            perror(fprintf);
+        if (fprintf(pids, "%d\n", pid) < SUCCESS) {
+            perror("fprintf");
             return FPRINTF_ERROR;
         }
         if (fflush(pids)) {
-            perror(fflush);
+            perror("fflush");
             return FFLUSH_ERROR;
         }
         if (fclose(pids)) {
-            perror(fclose);
+            perror("fclose");
             return FCLOSE_ERROR;
         }
         return SUCCESS;
@@ -48,28 +48,35 @@ int add_pid(int pid) {
 
 int main(int argc, char* argv[]) {
     if (argc != EXPECTED_NUM_ARGS) {
-        if (printf("Usage: %s <PID>", argv[PROGRAM_NAME]) < 0) {
-            perror(printf);
+        if (printf("Usage: %s <PID>\n", argv[PROGRAM_NAME]) < SUCCESS) {
+            perror("printf");
             return PRINTF_ERROR;
         }
         if(fflush(stdout)) {
-            perror(fflush);
+            perror("fflush");
             return FFLUSH_ERROR;
         }
         return USAGE_ERROR;
     }
     int pid = atoi(argv[PID]);
     if (pid == ATOI_RET_ERROR_VALUE) {
-        perror(atoi);
-        return ATOI_ERROR;
-    }
-    if (add_pid(pid) < 0) {
-        if (printf("Error with add_pid!\n") < 0) {
-            perror(printf);
+        if (printf("Usage: %s <PID>\n", argv[PROGRAM_NAME]) < SUCCESS) {
+            perror("printf");
             return PRINTF_ERROR;
         }
         if(fflush(stdout)) {
-            perror(fflush);
+            perror("fflush");
+            return FFLUSH_ERROR;
+        }
+        return USAGE_ERROR;
+    }
+    if (add_pid(pid) < SUCCESS) {
+        if (printf("Error with add_pid!\n") < SUCCESS) {
+            perror("printf");
+            return PRINTF_ERROR;
+        }
+        if(fflush(stdout)) {
+            perror("fflush");
             return FFLUSH_ERROR;
         }
         return ADD_PID_ERROR;
