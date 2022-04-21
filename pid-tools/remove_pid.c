029ed5d72a1857116a88c493cd44e1b8a345060a
diva-harsoor <divaharsoor@gmail.com>
2022-04-21 15:40:18 -0500

    Debugging

--- pid-tools/remove_pid.c
+++ pid-tools/remove_pid.c
@@ -1,17 +1,20 @@
 #include "pid_user_tools.h"
 
 int remove_pid(char* pid) {
-    if (access(".pids", F_OK) == SUCCESS) {
+    if (access(".pids", F_OK) != SUCCESS) {
+         return NO_DOT_PIDS_FILE;
+    }
+    else {
       FILE * pidfile;
       pidfile = fopen(".pids", "a+");
       if (!pidfile) {
-        perror(fopen);
+        perror("fopen");
         return FOPEN_ERROR;
       }
       FILE * temp;
       temp = fopen("temp", "w+");
       if (!temp) {
-        perror(fopen);
+        perror("fopen");
         return FOPEN_ERROR;
       }
 
@@ -21,82 +24,81 @@ int remove_pid(char* pid) {
 
       buf = (char *)malloc(bufsize * sizeof(char));
       if (buf == NULL) {
-        perror(malloc);
+        perror("malloc");
         return MALLOC_ERROR;
       }
-
-
-      while ((read = getline(&buf, &bufsize, pidfile)) > SUCCESS) {
+        
+           while ((read = getline(&buf, &bufsize, pidfile)) > SUCCESS) {
         buf[strlen(buf) - 1] = '\0';
 
         if (strcmp(buf, pid) != SUCCESS) {
           if (fputs(buf, temp) == EOF) {
-            perror(fputs);
+            perror("fputs");
             return FPUTS_ERROR;
           }
           if (fputc('\n', temp) == EOF) {
-            perror(fputc);
+            perror("fputc");
             return FPUTC_ERROR;
           }
         }
         if(fflush(temp)) {
-          perror(fflush);
+          perror("fflush");
           return FFLUSH_ERROR;
         }
       }
-
-      lseek(fileno(temp), 0, SEEK_SET);
+        
+              lseek(fileno(temp), 0, SEEK_SET);
       pidfile = freopen(".pids", "w", pidfile);
       while ((read = getline(&buf, &bufsize, temp)) > SUCCESS) {
         if (fputs(buf, pidfile) == EOF) {
-          perror(fputs);
+          perror("fputs");
           return FPUTS_ERROR;
         }
       }
       if (fclose(pidfile)) {
-        perror(fclose);
+        perror("fclose");
         return FCLOSE_ERROR;
       }
       if (fclose(temp)) {
-        perror(fclose);
+        perror("fclose");
         return FCLOSE_ERROR;
       }
 
       if (remove("temp") != SUCCESS) {
-        perror(remove);
+        perror("remove");
         return REMOVE_FILE_ERROR;
       }
 
     }
-  }
   return SUCCESS;
 }
 
-// add malloc_error, fputs_error, fputc_error, remove_error, and unistd.h
 int main (int argc, char *argv[]) {
   if (argc != EXPECTED_NUM_ARGS) {
     if (printf("Usage: %s <PID>", argv[PROGRAM_NAME]) < SUCCESS) {
-      perror(printf);
+      perror("printf");
       return PRINTF_ERROR;
     }
     if (fflush(stdout)) {
-      perror(printf);
+      perror("printf");
       return FFLUSH_ERROR;
     }
     return USAGE_ERROR;
   }
 
-  if (remove_pid(argv[PID] < SUCCESS) {
-    if (printf("Error with remove_pid!\n" < SUCCESS) {
-      perror(printf);
+  if (remove_pid(argv[PID]) < SUCCESS) {
+    if (printf("Error with remove_pid!\n") < SUCCESS) {
+      perror("printf");
       return PRINTF_ERROR;
     }
     if (fflush(stdout)) {
-      perror(fflush);
+      perror("fflush");
       return FFLUSH_ERROR;
     }
     return REMOVE_PID_ERROR;
-    }
 
+  }
+  else {
     return SUCCESS;
   }
+}
