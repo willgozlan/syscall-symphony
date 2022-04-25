d5f751f90071eef706a5560318048b3e154a97a9
diva-harsoor <divaharsoor@gmail.com>
2022-04-25 17:29:59 -0500

    Moving includes

--- misc/readline.h
+++ misc/readline.h
@@ -4,8 +4,6 @@
 #include <fcntl.h>
 //#include <unistd.h>
 //#include <sys/syscall.h>
-#include <sys/file.h>
-#include <fcntl.h>
 
 int readline (char *buf, int sz, const char *fn, off_t *offset);
 
