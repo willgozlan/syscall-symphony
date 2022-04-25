e6249a409aca2b584cf9e5f7db02e1047597baf2
diva-harsoor <divaharsoor@gmail.com>
2022-04-25 17:15:44 -0500

    Added locking

--- misc/readline.h
+++ misc/readline.h
@@ -1,9 +1,12 @@
-
 #pragma once
 
 //#include <stdio.h>
 #include <fcntl.h>
 //#include <unistd.h>
 //#include <sys/syscall.h>
+#include <sys/file.h>
+#include <fcntl.h>
 
 int readline (char *buf, int sz, const char *fn, off_t *offset);
+
+#define FLOCK_ERROR 1
