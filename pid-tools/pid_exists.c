35c394d5acbb421fc698e8a76a15cfb807da7186
diva-harsoor <divaharsoor@gmail.com>
2022-04-25 17:22:22 -0500

    Update pid_exists.c

--- pid-tools/pid_exists.c
+++ pid-tools/pid_exists.c
@@ -17,7 +17,7 @@ int pid_exists(int pid)
     int len = 0;
 
     // Using readline to read each line of file ".pids" into "line" 
-    while ((len = readline(line, BUF_SIZE, /syscall-symphony/pid-tools/.pids", &offset)) > 0)
+    while ((len = readline(line, BUF_SIZE, "/syscall-symphony/pid-tools/.pids", &offset)) > 0)
     {
         // Check if PID is present on current line
         if(my_atoi(line) == pid)
