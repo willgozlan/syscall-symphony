110cef5598f4251bbf5ce905dd4cbbdcd4cef4ba
William Gozlan <46691531+willgozlan@users.noreply.github.com>
2022-04-25 18:00:29 -0500

    Update readline.h

--- misc/readline.h
+++ misc/readline.h
@@ -1,11 +1,5 @@
 #pragma once
 
-//#include <stdio.h>
 #include <fcntl.h>
-//#include <unistd.h>
-//#include <sys/syscall.h>
 
 int readline (char *buf, int sz, const char *fn, off_t *offset);
-
-#define SUCCESS 1
-#define FLOCK_ERROR 1
