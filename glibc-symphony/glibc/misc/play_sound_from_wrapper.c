fe1db446ee6372bacc4cbd8cab7e2e4f3f1a0087
diva-harsoor <divaharsoor@gmail.com>
2022-05-03 15:44:49 -0500

    Added comments

--- glibc-symphony/glibc/misc/play_sound_from_wrapper.c
+++ glibc-symphony/glibc/misc/play_sound_from_wrapper.c
@@ -1,8 +1,20 @@
+/*
+ * File Name: play_sound_from_wrapper.c
+ * File Summary
+ *  - Checks if the given PID has sound privileges
+ *  - If it does, calls play_sound() with the given syscall
+ */
+
 #include <stdio.h>
 #include "play_sound_from_wrapper.h"
 
 #define SUCCESS 0
 
+/*
+ * Checks if the given PID has sound privileges
+ * If it does, calls play_sound() with the given syscall
+ * Returns success or a unique error value
+ */
 void play_sound_from_wrapper(int pid, int syscall)
 {
 	int pid_exists_ret = pid_exists(pid);
