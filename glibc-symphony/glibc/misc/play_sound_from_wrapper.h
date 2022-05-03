74176b261b60d5d179e13e535d4a1c12c39adbc2
diva-harsoor <divaharsoor@gmail.com>
2022-05-03 15:45:27 -0500

    Added comments

--- glibc-symphony/glibc/misc/play_sound_from_wrapper.h
+++ glibc-symphony/glibc/misc/play_sound_from_wrapper.h
@@ -1,4 +1,10 @@
+/*
+ * File Name: play_sound_from_wrapper.h
+ * File Summary: Header file for play_sound_from_wrapper.c
+ */
+
 #pragma once
+
 #include "play_sound.h"
 #include "pid_exists.h"
 #include <sys/types.h>
